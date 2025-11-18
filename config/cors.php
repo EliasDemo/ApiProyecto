f1f8f91cc1ff995b8e38152936030b9c25be4d28
EliasDemo <franckalbertsoncoaquirajusto@gmail.com>
2025-11-18 08:44:56 -0500

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,21 +1,31 @@
 <?php
 
 return [
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-        'allowed_methods' => ['*'],
-        'allowed_origins' => [
-        'http://localhost',
-        'https://localhost',
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        // Angular local
         'http://localhost:4200',
-        'https://localhost:4200',
-        'http://127.0.0.1',
-        'http://127.0.0.1:4200',
-        '*', // si quieres permitir todo durante pruebas
+        'http://172.31.208.1:4200',
+
+        // Lo que te está saliendo en el error del navegador
+        'https://localhost',
+
+        // Cuando la app corre dentro de Capacitor (Android/iOS)
+        'capacitor://localhost',
     ],
 
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => false, // usa true solo si trabajas con cookies/sesiones
+
+    // Como usas token en localStorage y no cookies, esto en false está bien
+    'supports_credentials' => false,
 ];
