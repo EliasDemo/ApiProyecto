c4822e22d19d747d923e761fa794fe585c5e5dae
EliasDemo <franckalbertsoncoaquirajusto@gmail.com>
2025-11-19 03:08:35 -0500

    seeder

--- config/cors.php
+++ config/cors.php
@@ -3,13 +3,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:4200',
-        'http://192.168.0.198:4200',
-    ],
+    'allowed_origins' => ['*'],  // permitir cualquier origen
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false, // usa true solo si trabajas con cookies/sesiones
+    'supports_credentials' => false,
 ];
+
